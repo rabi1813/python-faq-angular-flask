b7b9823f10766c20db92f1ae790b0b775b25f345
rabi1813 <rabindra1813@gmail.com>
2023-01-08 23:00:23 +0530

    Config

---
+++ backend/get_connection.py
@@ -0,0 +1,13 @@
+import psycopg2
+import psycopg2.extras
+
+
+def db_connection():
+    connection = psycopg2.connect(dbname="postgres",
+                                  user="postgres",
+                                  password="Rabi@1991",
+                                  host="host.docker.internal",
+                                # host="localhost",
+                                  port='5432',
+                                  cursor_factory=psycopg2.extras.RealDictCursor)
+    return connection
